5da04b7304a647152ed6aa965d7f0623d9b1cbe3
Diabetes_Prediction <manuachu0611@gmail.com>
2024-12-23 07:51:16 +0530

    error modified1

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def hello():
     return "This website is in development phase"
 
-if (__name__) == (__main__):
+if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
